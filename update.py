30ee26759374850f522e7a26f702a2054215a84d
woliver99 <oliver@maplenetwork.ca>
2024-08-05 22:47:06 -0400

    Close files and custom account and list id

--- update.py
+++ update.py
@@ -10,12 +10,15 @@ last_list_file = "./last_list.json"
 def get_ip() -> str:
     url = "https://api.ipify.org/"
 
-    response = requests.request("GET", url)
+    try:
+        response = requests.request("GET", url)
+    except:
+        raise Exception("Failed to get IP. Check your internet connection and try again.")
 
-    if response.status_code == 200:
-        return response.text
+    if response.status_code != 200:
+        raise Exception("Failed to get IP with status code: " + str(response.status_code))
     
-    raise Exception("Failed to get IP with status code: " + str(response.status_code))
+    return response.text
 
 def get_secrets() -> dict:
     secrets_file = "./secrets.json"
@@ -24,28 +27,34 @@ def get_secrets() -> dict:
         # Create secrets file with default values
         default_secrets = {
             "email": "your_email@example.com",
-            "key": "your_api_key"
+            "key": "your_api_key",
+            "account_id": "your_account_id",
+            "list_id": "your_list_id"
         }
         with open(secrets_file, "w") as f:
             json.dump(default_secrets, f)
-        print("Secrets file created with default values. Now is the time to enter your email and key into the file.")
+        print("Secrets file created with default values. Now is the time to enter your email, key, account_id, and list_id into the file.")
         input("Press Enter to continue... ")
 
     # Read secrets from file
     try:
         with open(secrets_file, "r") as f:
             secrets = json.load(f)
+            f.close()
     except:
         raise Exception("Failed to read secrets file. Try deleting it and running the script again for default configuration")
 
-    if "email" not in secrets or "key" not in secrets:
-        raise Exception("Email or key not found in secrets file. Try deleting it and running the script again for default configuration")
+    required_keys = ["email", "key", "account_id", "list_id"]
+    for key in required_keys:
+        if key not in secrets:
+            raise Exception(f"{key} not found in secrets file. Try deleting it and running the script again for default configuration")
     
     return secrets
 
 def update_ip(ip: str) -> bool:
-    account_id = "7f43d57de6f3aca69f908ca66f660465"
-    list_id = "5ae25761e55640d89721b8cf1df7076e"
+    secrets = get_secrets()
+    account_id = secrets["account_id"]
+    list_id = secrets["list_id"]
     url = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/rules/lists/{list_id}/items"
 
     payload = [
@@ -54,7 +63,6 @@ def update_ip(ip: str) -> bool:
         }
     ]
 
-    secrets = get_secrets()
     headers = {
         "Content-Type": "application/json",
         "X-Auth-Email": secrets["email"],
@@ -74,13 +82,16 @@ def update_ip(ip: str) -> bool:
 def store_last_list(new_list: set) -> None:
     with open(last_list_file, "w") as f:
         json.dump(list(new_list), f)
+        f.close()
 
 def get_last_list() -> set:
     if not os.path.exists(last_list_file):
         return set()
     
     with open(last_list_file, "r") as f:
-        return set(json.load(f))
+        last_list = set(json.load(f))
+        f.close()
+        return last_list
 
 print("Started IP update script...")
 while True:
